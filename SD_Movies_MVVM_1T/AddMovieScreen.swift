dc575500dba6db5e401e8ffa03bb58fec064731c
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-11-03 17:30:06 +0100

    WIP MoviesVM

--- SD_Movies_MVVM_1T/AddMovieScreen.swift
+++ SD_Movies_MVVM_1T/AddMovieScreen.swift
@@ -5,16 +5,36 @@ import SwiftUI
 
 struct AddMovieScreen: View {
     
-//    @State
+    @State private var vm = MoviesVM()
+    
+    @State private var selectedDate = Date()
     
     var body: some View {
         Form {
-            Text("hello world")
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Title").font(.headline)
+                TextField("Title", text: $vm.title)
+            }
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Year").font(.headline)
+                Picker("Year", selection: $vm.year) {
+                    ForEach(vm.years, id: \.self) { year in
+                        Text("\(year)")
+                            .tag(year)
+                    }
+                }
+                .pickerStyle(.wheel)
+                
+            }
+            
         }
+        .navigationTitle("Add Movie")
     }
 }
 
 
 #Preview {
-    AddMovieScreen()
+    NavigationStack {
+        AddMovieScreen()
+    }
 }
--- SD_Movies_MVVM_1T/MoviesVM.swift
+++ SD_Movies_MVVM_1T/MoviesVM.swift
@@ -4,4 +4,21 @@
 import Foundation
 import Observation
 
+@Observable final class MoviesVM {
+    var title: String = ""
+    var year: Int
+    let years: [Int]
+    
+    private let minYear = 1850
+    private let maxYear = Calendar.current.component(.year, from: .now)
 
+    init() {
+        years = Array(minYear...maxYear)
+        year = maxYear - 5
+    }
+    
+    func setDefaultValues() {
+        title = ""
+        year = maxYear - 5
+    }
+}
